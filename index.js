541e988564a1cd7b538169b7d7488bd58c759134
rpulkka <dreamspace2017@yandex.ru>
2019-11-04 16:51:47 +0200

    3.4 finished.

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
 app.get('/info', (req, res) => {
   const text = 'Phonebook has info for ' + persons.length + ' people<br/><br/>' + new Date()
   res.send(text)
