17de180e10019ef88303886fe927501caf7762b9
rpulkka <dreamspace2017@yandex.ru>
2020-01-07 08:59:09 +0200

    Deletes objects from DB.

--- index.js
+++ index.js
@@ -74,11 +74,12 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
